03a25e18ebeb4baa29fc4a4a2e53ba0396ea941c
Phuc Hoang To <me@phuc.to>
2023-04-10 11:36:08 +0700

    Remove validators folder

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -1,13 +1,8 @@
-/* eslint-disable object-curly-newline */
-const { validationResult } = require('express-validator');
 const eventService = require('../services/eventService');
-const { addEventValidator } = require('../validator/addEventValidator');
-const EventAlreadyExistsError = require('../errors/eventAlreadyExistsError');
 
 module.exports = {
   manageEvent: async (req, res) => {
     const events = await eventService.getAllEvents();
     return res.render('dashboard/events', { title: 'Manage Events', events });
   },
-
 };
